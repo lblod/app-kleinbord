82111a6dcd1c4a8c346cbac931ebcac2e5cff967
Claire Lovisa <lovisa.claire@gmail.com>
2019-10-04 16:39:47 +0200

    Add roadsign resources and data to the stack

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -26,6 +26,26 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://snippets/"
   end
 
+  match "/verkeersbordcombinaties/*path" do
+    Proxy.forward conn, path, "http://resource/verkeersbordcombinaties/"
+  end
+
+  match "/maatregelconcepten/*path" do
+    Proxy.forward conn, path, "http://resource/maatregelconcepten/"
+  end
+
+  match "/verkeersbordconcepten/*path" do
+    Proxy.forward conn, path, "http://resource/verkeersbordconcepten/"
+  end
+
+  match "/verkeersbordcategorieen/*path" do
+    Proxy.forward conn, path, "http://resource/verkeersbordcategorieen/"
+  end
+
+  match "/verkeersbordconcept-status-codes/*path" do
+    Proxy.forward conn, path, "http://resource/verkeersbordconcept-status-codes/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
